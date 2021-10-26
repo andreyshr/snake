a9460bc5bfa7a2b7c558037676daf4ad4b26dbfa
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2021-10-26 09:00:07 +0300

    добавлены 3 базовых уровня игры

---
+++ src/game/levels/index.ts
@@ -0,0 +1,6 @@
+import level_1 from "./1.json";
+import level_2 from "./2.json";
+import level_3 from "./3.json";
+import { Position } from "../core/types";
+
+export const levels = [level_1, level_2, level_3] as Position[][];
