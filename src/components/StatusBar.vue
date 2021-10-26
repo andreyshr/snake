a080160df1824ce214bfadac71a6c4fcf59e9aa5
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2021-10-26 09:44:57 +0300

    подготовлен компонент StatusBar

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="status-bar">
+    <div>
+      <span>scores: </span>
+      <span>{{ scores }}</span>
+    </div>
+    <base-button
+      @click="$emit('paused')"
+      :disabled="!isPlaying || isPaused"
+      class="status-bar__pause_button"
+    >
+      <base-icon icon="pause"></base-icon>
+    </base-button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import BaseButton from "@/components/BaseButton";
+import BaseIcon from "@/components/BaseIcon";
+
+export default defineComponent({
+  name: "StatusBar",
+  components: { BaseIcon, BaseButton },
+  props: {
+    isPlaying: Boolean,
+    isPaused: Boolean,
+    scores: Number,
+  },
+  emits: ["paused"],
+});
+</script>
+
+<style scoped>
+.status-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 8px;
+  font-size: 24px;
+  align-self: flex-start;
+}
+
+.status-bar__pause_button {
+  width: auto;
+  padding: 0;
+}
+</style>
