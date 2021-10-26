c5f19e1b702c2d9235c260051f93234a3358fc5b
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2021-10-26 09:43:03 +0300

    подготовлен компонент BaseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="Icon" />
+</template>
+
+<script lang="ts">
+import { defineComponent, defineAsyncComponent, computed } from "vue";
+
+export default defineComponent({
+  name: "BaseIcon",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const getIcon = () => import(`@/assets/icons/${props.icon}.svg`);
+
+    const Icon = computed(() => defineAsyncComponent(getIcon));
+
+    return {
+      Icon,
+    };
+  },
+});
+</script>
