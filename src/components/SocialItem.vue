015f7b6a5db6683203d771ff096038f62f61163e
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2021-10-26 09:44:16 +0300

    подготовлены компоненты SocialList, SocialItem

---
+++ src/components/SocialItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <a class="social-item" :href="item.link" target="_blank">
+    {{ item.title }}
+    <base-icon :icon="item.icon" />
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import BaseIcon from "@/components/BaseIcon.vue";
+
+export type TSocialItem = {
+  title: string;
+  link: string;
+  icon: string;
+};
+
+export default defineComponent({
+  name: "SocialItem",
+  components: { BaseIcon },
+  props: {
+    item: {
+      type: Object as PropType<TSocialItem>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.social-item {
+  display: flex;
+  align-items: center;
+  color: var(--color-secondary);
+  font-size: 20px;
+}
+</style>
---
+++ src/components/SocialList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="social-list">
+    <social-item v-for="(item, i) in items" :key="i" :item="item" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SocialItem, { TSocialItem } from "@/components/SocialItem.vue";
+
+export default defineComponent({
+  name: "SocialList",
+  components: { SocialItem },
+  setup() {
+    const items: TSocialItem[] = [
+      {
+        title: "github",
+        link: "https://github.com/andreyshr/snake",
+        icon: "github",
+      },
+    ];
+
+    return {
+      items,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.social-list {
+  display: flex;
+  align-items: center;
+  position: fixed;
+  right: 10px;
+  top: 10px;
+
+  @media (min-width: 768px) {
+    right: 20px;
+    bottom: 20px;
+    top: auto;
+  }
+}
+</style>
