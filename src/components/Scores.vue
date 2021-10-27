caaf5d88dba35761478d3977e65bff52c2553755
Andrey Shaurov <Andrey.Shaurov@miningelement.com>
2021-10-27 17:05:28 +0300

    подготовлен компонент Scores

---
+++ src/components/Scores.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="scores">
+    <span class="scores__title">scores: </span>
+    <span class="scores__quantity">{{ scores }}</span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Scores",
+  props: {
+    scores: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
+</script>
